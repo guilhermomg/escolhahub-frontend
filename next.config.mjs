ae4457b2afbdf476abaa1c0bb8d17a7ccb98706b
Guilhermo González <guilhermo@gonzalez.dev.br>
2026-03-28 20:52:58 -0400

    Repository rename

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/alunio-frontend',
+  basePath: '/escolhahub-frontend',
   typescript: {
     ignoreBuildErrors: true,
   },
